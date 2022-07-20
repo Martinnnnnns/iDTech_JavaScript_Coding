52ccc442e59011c0a5cbf80139246b98a9008aa1
Martinnnnnns <bernardomloguterres@gmail.com>
2022-07-20 16:23:39 +0100

    Updated all files

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,33 @@
+// important aspects of the story
+
+let name = "Bernardo";
+let sisname = "Joyce";
+let series = "A Song of Ice and Fire";
+let show = "Game of Thrones";
+
+// information about the seasons the books and the time
+let seasons = 8
+let books = 5
+let time = 15
+
+// characters from Game of Thrones
+let characters = ["John Snow", "Daenerys  Targaryen", "Arya Stark", "Jaime Lannister", "Samwell Tarly"]
+
+// it will be better to think of true with a capital T in the actual story.
+let haveuread = true
+
+// For additional context, I am watching game of thrones late at night when my fictional older sister Joyce enters the room.
+console.log(name + ": This show so addicting, way better than the book series!");
+console.log(sisname + ": What, "  + show + "? But you haven't even read the first book!!");
+console.log(name + ":" + haveuread + ", but I've always preferred watching over reading!");
+console.log(sisname + "(sighs):" + " You should give it a try! ");
+console.log(sisname + ": " + series + " is a great book series you'd surely enjoy!");
+console.log(name + ": I'm almost done with the " + seasons + " seaons of " + show + ",");
+console.log(name + ": so I could start with the " + books + " books of the series!");
+console.log(sisname + ": Who's your favourite character?");
+console.log(name + ": I'd definitely say my favourite is " + characters[0] + " as he's strong and brave. How about you?");
+console.log(sisname + ": My favourite character is " + characters[4] + ", he's so kind and wise.");
+console.log(sisname + ": Well, I'm gonna go sleep, dont go to sleep too late!");
+console.log(name + ": I'll head to bed in " + time + "." + " Goodnight " + sisname + "!");
+console.log(sisname + ": Goodnight " + name + "!");
+
